5cc71b4e39f97712b4b8da5a3f2c45e189979cf8
yegnasivasai <yegnasivasai@gmail.com>
2025-01-14 20:48:22 +0530

    Updated String reverse

--- src/main/java/org/engineering/digest/reverseString.java
+++ src/main/java/org/engineering/digest/reverseString.java
@@ -1,22 +1,27 @@
 package org.engineering.digest;
 
+import java.util.Scanner;
+
 public class reverseString {
 
 
     public static String reverse(String str){
-
+        String reversedString = "";
         int length_of_string = str.length();
         for (int i = length_of_string-1; i>=0; i--){
-            System.out.println(str.charAt(i));
+            reversedString = reversedString + str.charAt(i);
+//            System.out.println(str.charAt(i));
         }
-        return "";
+        return reversedString;
     }
 
 
 
     public static void main(String[] args) {
 
-        String str = "hello";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String..:  ");
+        String str = sc.next();
         System.out.println(reverse(str));
     }
 }
