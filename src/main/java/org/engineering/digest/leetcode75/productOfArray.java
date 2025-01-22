ae7ddfe6eec9b3ac4bf647df80c384b48ca2bc3e
yegnasivasai <yegnasivasai@gmail.com>
2025-01-22 21:16:42 +0530

    onJan22

---
+++ src/main/java/org/engineering/digest/leetcode75/productOfArray.java
@@ -0,0 +1,33 @@
+package org.engineering.digest.leetcode75;
+
+import java.util.Arrays;
+
+public class productOfArray {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        left[0] =1;
+        for (int i =1; i< nums.length; i++){
+            left[i] = left[i-1] * nums[i-1];
+        }
+
+        right[nums.length-1] =1;
+        for (int i = nums.length-2; i> -1; i--){
+            right[i] = right[i+1] * nums[i+1];
+        }
+
+        for (int i =0; i< nums.length; i++){
+            ans[i] = left[i] * right[i];
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+}
